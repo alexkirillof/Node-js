f309501c603071be405ca30975195dc516a47480
Андрей Разумов <alexkirillof@yandex.ru>
2022-03-24 22:09:17 +0300

    Lesson_2

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat.js';
-import duration from 'dayjs/plugin/duration.js';
-import EventEmitter from 'events';
-import process from 'process';
+const EventEmitter = require('events'),
+    emitter = new EventEmitter();
+require('moment-precise-range-plugin');
+const moment = require('moment');
 
-dayjs.extend(duration);
+const [userPastDate] = process.argv.slice(2);
+const format = 'YYYY-MM-DD HH:mm:ss';
 
-const setTimers = process.argv.slice(2);
-const emitter = new EventEmitter();
+const getDateFromDateString = (dateString) => {
+    const [sec, min, hour, day, month, year] = dateString.split('-');
+    return new Date(Date.UTC(year, month - 1, day, hour, min, sec));
+};
 
-class Timer {
-    constructor(timerValue) {
-        this.endTime = this.getTimerEndTime(timerValue);
-        this.timerValue = timerValue;
+const dateInFuture = getDateFromDateString(userPastDate);
 
-        this.interval = setInterval(() => {
-            this.timerTick();
-        }, 1000);
+// console.log(dateInFuture);
+
+const showRemainingTime = (dateInFuture) => {
+    const dateNow = new Date();
+
+    if (dateNow >= dateInFuture) {
+        emitter.emit('timerEnd');
+    } else {
+        const currentDateFormatted = moment(dateNow, format);
+        const futureDateFormatted = moment(dateInFuture, format);
+        const diff = moment.preciseDiff(currentDateFormatted, futureDateFormatted);
+
+        console.log(diff);
     }
+};
 
-    getTimerEndTime(timerValue) {
-        const timerToArrayOfValues = timerValue.split('-');
-        if (timerToArrayOfValues.length < 6) {
-            throw new Error('Неверный формат продолжительности таймера');
-        }
-        const now = dayjs();
-        const timerDuration = dayjs.duration({
-            seconds: timerToArrayOfValues[0],
-            minutes: timerToArrayOfValues[1],
-            hours: timerToArrayOfValues[2],
-            days: timerToArrayOfValues[3],
-            months: timerToArrayOfValues[4],
-            years: timerToArrayOfValues[5],
-        });
-        return now.add(timerDuration);
-    }
+const timerId = setInterval(() => {
+    emitter.emit('timerTick', dateInFuture);
+}, 1000)
 
-    timerTick() {
-        const duration = dayjs.duration(this.endTime.diff(dayjs()));
-        if (duration.asSeconds() <= 0) {
-            emitter.emit('timerFinish', `Таймер ${this.timerValue} завершился`);
-            clearInterval(this.interval);
-        } else {
-            emitter.emit('timerTick', `До таймера ${this.timerValue} осталось ${duration.format('YY лет MM месяцев DD дней HH:mm:ss')}`);
-        }
-    }
-}
+const showTimerDone = (timerId) => {
+    clearInterval(timerId);
+    console.log('End');
+};
 
-setTimers.forEach(inputTimer => {
-    new Timer(inputTimer);
-});
-
-emitter.on('timerFinish', (payload) => {
-    console.log(payload);
-});
-
-emitter.on('timerTick', (payload) => {
-    console.log(payload);
+emitter.on('timerTick', showRemainingTime);
+emitter.on('timerEnd', () => {
+    showTimerDone(timerId);
 });
